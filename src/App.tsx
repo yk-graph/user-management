f789833e6bf383702d6a1ccdef135ad944d5ff70
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-14 17:38:06 +0900

    feat: ルーティングの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React, { FC } from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter } from 'react-router-dom'
+
 import theme from './theme'
+import Router from './router'
 
 const App: FC = () => {
-  return <ChakraProvider theme={theme}></ChakraProvider>
+  return (
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ChakraProvider>
+  )
 }
 
 export default App
---
+++ src/components/pages/Page404.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react'
+
+const Page404: FC = () => {
+  return <div>URLが見つかりません</div>
+}
+
+export default Page404
