3e2f53b280ed9244b0729eb27b9d9e973f00be8d
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-15 04:13:03 +0900

    refactor: メッセージ表示機能の実装

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,10 +2,12 @@ import { useState, useCallback } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
+import { useMessage } from './useMessage'
 import { UserType } from '../types/api/user'
 
 export const useAuth = () => {
   const navigate = useNavigate()
+  const { showMessage } = useMessage()
 
   // ローディングの制御
   const [loading, setLoading] = useState(false)
@@ -22,12 +24,15 @@ export const useAuth = () => {
         .get<UserType>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            showMessage({ title: 'ログインしました', status: 'success' })
             navigate('/home')
           } else {
-            alert('ユーザーがみつかりません')
+            showMessage({ title: 'ユーザーがみつかりません', status: 'error' })
           }
         })
-        .catch((error) => alert('ログインできませんでした'))
+        .catch(() =>
+          showMessage({ title: 'ログインできませんでした', status: 'error' })
+        )
         // 正常に処理が終わってもエラーになっても、最終的にローディングは終了させたいためfinalyで処理を記述
         .finally(() => setLoading(false))
     },
---
+++ src/hooks/useMessage.ts
@@ -0,0 +1,31 @@
+import React, { useCallback } from 'react'
+import { useToast } from '@chakra-ui/react'
+
+interface Props {
+  title: string
+  status: 'info' | 'warning' | 'success' | 'error'
+}
+
+export const useMessage = () => {
+  const toast = useToast()
+
+  /**
+   *受け取ったpropsを元にフラッシュメッセージ(toast)を表示させる
+   *@param {String} title 表示させたいメッセージ
+   *@param {String} status 表示させたいメッセージのステータス
+   */
+  const showMessage = useCallback(
+    (props: Props) => {
+      const { title, status } = props
+      toast({
+        title,
+        status,
+        position: 'top',
+        duration: 2000,
+        isClosable: true,
+      })
+    },
+    [toast]
+  )
+  return { showMessage }
+}
