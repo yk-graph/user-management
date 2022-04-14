8c364f859e718c6b295184b7f61ce26ed44dbf0d
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-14 16:18:10 +0900

    reset

--- src/components/pages/Page404.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Page404: React.FC = () => {
-  return <div>ページが見つかりません</div>
-}
-
-export default Page404
--- src/components/pages/UserManagement.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const UserManagement: React.FC = () => {
-  return <div>UserManagement</div>
-}
-
-export default UserManagement
